83d7716f39b0308c3316dd5d1c01a9a258d40d10
TAKE Takashi <705098+take-takashi@users.noreply.github.com>
2024-03-20 07:46:11 +0000

    設定の追加

--- .devcontainer/postCreateCommand.bash
+++ .devcontainer/postCreateCommand.bash
@@ -5,7 +5,11 @@ WORKSPACE=$PWD
 # __pycache__フォルダを作成しない設定
 echo "export PYTHONDONTWRITEBYTECODE=1" >> ~/.bashrc
 
-# sample_dashのセットアップ
-# cd ${WORKSPACE}/sample_dash
-# poetry install --no-root
-# poetry shell &
+# セットアップ
+cd ${WORKSPACE}/sample_project01
+poetry install --no-root
+poetry shell &
+
+cd ${WORKSPACE}/sample_project02
+poetry install --no-root
+poetry shell &
---
+++ sample-project02/README.md
@@ -0,0 +1,7 @@
+# Setup commands
+
+## Poetry commands
+
+```sh
+@take-takashi ➜ /workspaces/try-nest-poetry (main) $ poetry new sample-project02 --src
+```
